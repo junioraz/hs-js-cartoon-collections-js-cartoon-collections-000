feb60c9f7edbd475893023fac5dff97a3314d771
junior az <youness.azzab27@gmail.com>
2020-01-19 09:34:28 +0000

    Done.

--- index.js
+++ index.js
@@ -1,11 +1,44 @@
-function dwarfRollCall(dwarves) {
+function dwarfRollCall(dwarves) { 
+  let dwarfNames = "";
+  for (let i = 0; i < dwarves.length; i++) {
+    let progression = i +1;
+    dwarfNames += progression + ". " + dwarves[i] + " ";
+  }
+  return dwarfNames;
+  
 }
 
 function summonCaptainPlanet(planeteerCalls){
+  for (let i = 0; i < planeteerCalls.length; i++) {
+    planeteerCalls[i] = planeteerCalls[i].toUpperCase() + "!";
+  }
+  return planeteerCalls 
 }
 
+
+
+
 function longPlaneteerCalls(words) {
+  for (let i = 0; i < words.length; i++){
+    if(words[i].length > 4){
+    return true;
+  }
+  else return false;
+}
 }
 
 function findTheCheese (foods) {
+  for (let i = 0; i < foods.length; i++){
+    if(foods[i] == "cheddar"|| foods[i] == "gouda" || foods[i] == "camembert"){
+      return foods[i];
+    }
+  }
+  return "no cheese!";
 }
+  
+  
+  
+  
+
+  
+
