b9f409fa7ee502f64a4b4144a4f452b56f97d72a
junior az <youness.azzab27@gmail.com>
2020-01-27 10:28:01 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ function summonCaptainPlanet(planeteerCalls){
   for (let i = 0; i < planeteerCalls.length; i++) {
     planeteerCalls[i] = planeteerCalls[i].toUpperCase() + "!";
   }
-  return planeteerCalls 
+  return planeteerCalls
 }
 
 
@@ -28,17 +28,27 @@ function longPlaneteerCalls(words) {
 }
 
 function findTheCheese (foods) {
+  
   for (let i = 0; i < foods.length; i++){
-    if(foods[i] == "cheddar"|| foods[i] == "gouda" || foods[i] == "camembert"){
+    if(foods[i] =="cheddar"|| foods[i] =="gouda"||foods[i] =="camembert"){
       return foods[i];
     }
   }
   return "no cheese!";
 }
-  
-  
-  
-  
+
+
+
+  // words = [ "tank", "grapes", "boat", "bob"] => [ "boat", "bob" ]
+  // function wordsWithB(words) {
+  //   let bWords = []
+  //   for(let i = 0 ; i < words.length; i++){
+  //     if(words[i].startsWith('b')){
+  //       bWords.push(words[i])
+  //     }
+  //   }
+  //   return bWords
+  // }
 
   
 
